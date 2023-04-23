56cfea776b5c170845d3e78f0c2e72a434efa342
Ritikesh Singh <77789435+RitikeshSingh@users.noreply.github.com>
2023-04-23 09:30:05 +0530

    Added solution - LeetHub

--- Union of two arrays - GFG/union-of-two-arrays.py
+++ Union of two arrays - GFG/union-of-two-arrays.py
@@ -4,9 +4,9 @@ class Solution:
     #Function to return the count of number of elements in union of two arrays.
     def doUnion(self,a,n,b,m):
         a.extend(b)
-        m=set(a)
+        q=set(a)
         
-        return len(m)
+        return len(q)
         
         #code here
 
